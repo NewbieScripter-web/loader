3b1e943a3752aeee5b6993050eb281955356d8b0
NewbieScripter-web <rataratalol1@gmail.com>
2024-11-16 21:58:55 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,29 @@
+--[[██   ██ ██   ██ ███████ ██ ██      ██████  ███    ██     ████████  ██████  ██████  
+    ██  ██  ██   ██ ██      ██ ██     ██    ██ ████   ██        ██    ██    ██ ██   ██ 
+    █████   ███████ ███████ ██ ██     ██    ██ ██ ██  ██        ██    ██    ██ ██████  
+    ██  ██       ██      ██ ██ ██     ██    ██ ██  ██ ██        ██    ██    ██ ██      
+    ██   ██      ██ ███████ ██ ██      ██████  ██   ████        ██     ██████  ██      
+                                                                                 ]]--                                       
+
+--i removed some scripts that are no longer working. coming soon are going to be working again.
+
+
+if game.PlaceId == 648362523 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/main/breaking.lua"))()
+elseif game.PlaceId == 10179538382 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/main/bronx.lua"))()
+elseif game.PlaceId == 13864661000 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/main/break.lua"))()
+elseif game.PlaceId == 17625359962 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/main/rivals.lua"))()
+elseif game.PlaceId == 10449761463 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/main/strongest.lua"))()
+elseif game.PlaceId == 17651315739 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/main/horror.lua"))()
+elseif game.PlaceId == 116295913939017 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/Test-v3/refs/heads/main/NIXKrEJ.lua"))()
+elseif game.PlaceId == 16732694052 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/lua-testing/refs/heads/main/fisch.lua", true))()
+else
+    print("Game Not Supported!")
+end
